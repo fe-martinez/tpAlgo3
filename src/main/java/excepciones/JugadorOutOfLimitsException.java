7f7c0f02a242a3874f5a74f2a6ef4ae61abd843c
Fran <83876019+fe-martinez@users.noreply.github.com>
2022-11-10 17:27:20 -0300

    Update JugadorOutOfLimitsException.java

--- src/main/java/excepciones/JugadorOutOfLimitsException.java
+++ src/main/java/excepciones/JugadorOutOfLimitsException.java
@@ -1,6 +1,5 @@
 package excepciones;
 
-//Habría que ver qué pingo hace esta excepción ahre
 public class JugadorOutOfLimitsException extends Exception {
 	public JugadorOutOfLimitsException() {
 		super("El jugador no puede posicionarse por afuera del terreno");
