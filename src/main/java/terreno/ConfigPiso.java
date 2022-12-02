269d89f7956e4b4760b0a6954cef5d8a820201e3
Fran <83876019+fe-martinez@users.noreply.github.com>
2022-12-02 20:41:31 -0300

    Delete src directory

--- src/main/java/terreno/ConfigPiso.java
+++
@@ -1,9 +0,0 @@
-package terreno;
-
-import java.util.Map;
-
-public interface ConfigPiso {
-	public void crearConfiguracion();
-	public Map<Integer, Entidad> getTiendas();
-	public Entidad getTiendaPos(int posX);
-}
