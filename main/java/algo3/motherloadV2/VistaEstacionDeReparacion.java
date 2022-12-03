68ffdb4967984421dd44def8f52f610b461f96f2
Clara Ruano Frugoli <cruano@fi.uba.ar>
2022-12-03 16:00:48 -0300

    Update VistaEstacionDeReparacion.java

--- main/java/algo3/motherloadV2/VistaEstacionDeReparacion.java
+++ main/java/algo3/motherloadV2/VistaEstacionDeReparacion.java
@@ -1,8 +1,29 @@
 package algo3.motherloadV2;
 
-public class VistaEstacionDeReparacion implements VistaEntidad {
-	//Es igual a la estación de servicio pero con otros valores :P
-	public void mostrar() {
+import java.util.List;
+import javafx.scene.Group;
+import javafx.stage.Stage;
+import jugador.Jugador;
+import tiendas.EstacionDeReparacion;
+
+public class VistaEstacionDeReparacion extends VistaEstacionDeMantenimiento {
+	final static String PATH_FONDO = "../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/HealthStation.png";
+	static List<String> keys = List.of("5","10","25","50","Fill","Close");
+	EstacionDeReparacion tienda;
+	
+	//Esta lista es la que va
+	static List<String> imagePath = List.of("../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/Botones/50.png",
+			"../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/Botones/100.png",
+			"../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/Botones/200.png",
+			"../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/Botones/500.png",
+			"../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/Botones/full.png",
+			"../motherloadV2/src/rsc/Tiendas/EstacionDeReparacion/Botones/close.png");
+	
+	public VistaEstacionDeReparacion(Stage stage, Group root,EstacionDeReparacion estacionDeReparacion, Jugador pj) {
+		super(stage,root,estacionDeReparacion,pj,PATH_FONDO,imagePath,keys);
 		
+		
+		this.tienda = estacionDeReparacion;
 	}
+
 }
